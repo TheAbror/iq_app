665a965e1e84581de2cfd0aa42bb6efb07b148af
Elbarto <oyjddkvj@gmail.com>
2023-10-02 19:37:26 +0500

    shared preferences

--- lib/core/preference_services/preference_services.dart
+++ lib/core/preference_services/preference_services.dart
@@ -2,14 +2,24 @@ import 'package:iq_app/core/preference_services/sh_pref_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferencesServices {
-  static Future<String?> getToken() async {
+  static Future<String?> getName() async {
     final preferences = await SharedPreferences.getInstance();
-    return preferences.getString(ShPrefKeys.token);
+    return preferences.getString(ShPrefKeys.name);
   }
 
-  static Future<bool> saveToken(String token) async {
+  static Future<bool> saveName(String token) async {
     final preferences = await SharedPreferences.getInstance();
-    return preferences.setString(ShPrefKeys.token, token);
+    return preferences.setString(ShPrefKeys.name, token);
+  }
+
+  static Future<int?> getAge() async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.getInt(ShPrefKeys.age);
+  }
+
+  static Future<bool> saveAge(int age) async {
+    final preferences = await SharedPreferences.getInstance();
+    return preferences.setInt(ShPrefKeys.age, age);
   }
 
   static Future<bool> clearAll() async {
