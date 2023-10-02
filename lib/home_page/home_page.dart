430c559b33ad142af6ce8b5774914efa4c72c89c
Elbarto <oyjddkvj@gmail.com>
2023-10-02 21:13:59 +0500

    sign in page deco

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:iq_app/core/preference_services/preference_services.dart';
+import 'package:iq_app/core/roots/app_routes.dart';
 import 'package:iq_app/home_page/home_page_item.dart';
 
 class HomePage extends StatefulWidget {
@@ -139,7 +142,7 @@ class _SignInPageState extends State<SignInPage> {
   @override
   void initState() {
     Future.delayed(Duration.zero, () {
-      _showMyDialog();
+      // _showMyDialog();
     });
     super.initState();
   }
@@ -147,63 +150,107 @@ class _SignInPageState extends State<SignInPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
 
-  Future<void> _showMyDialog() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text(
-            'Personal Info',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                const Text(
-                  'Name',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                TextFormField(
-                  controller: _nameController,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Age',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                TextFormField(
-                  controller: _ageController,
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Save'),
-              onPressed: () async {
-                final name = _nameController.text.trim();
-                final age = _ageController.text.trim();
-                await PreferencesServices.saveName(name);
-                await PreferencesServices.saveAge(age);
-
-                CircularProgressIndicator();
-
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [],
-        ));
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              // Neumorphic(
+              //   style: NeumorphicStyle(
+              //       shape: NeumorphicShape.concave,
+              //       boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+              //       depth: 8,
+              //       lightSource: LightSource.topLeft,
+              //       color: Colors.grey),
+              //   child: NeumorphicText(
+              //     "I love flutter",
+              //     style: const NeumorphicStyle(
+              //       depth: 4, //customize depth here
+              //       color: Colors.white, //customize color here
+              //     ),
+              //     textStyle: NeumorphicTextStyle(
+              //       fontSize: 18, //customize size here
+              //       // AND others usual text style properties (fontFamily, fontWeight, ...)
+              //     ),
+              //   ),
+              // ),
+              const SizedBox(height: 70),
+
+              Center(
+                child: SvgPicture.asset(
+                  'assets/images/Component 1.svg',
+                  width: 350.w,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(height: 70),
+
+              ListBody(
+                children: <Widget>[
+                  const Text(
+                    'Name',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextFormField(
+                    controller: _nameController,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Age',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextFormField(
+                    controller: _ageController,
+                  ),
+                ],
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () async {
+                  final name = _nameController.text.trim();
+                  final age = _ageController.text.trim();
+                  await PreferencesServices.saveName(name);
+                  await PreferencesServices.saveAge(age);
+
+                  const CircularProgressIndicator();
+
+                  // ignore: use_build_context_synchronously
+                  Navigator.pushNamed(context, AppRoutes.homePage);
+                },
+                child: Container(
+                  height: 45,
+                  width: double.infinity,
+                  margin: EdgeInsets.only(left: 16.w),
+                  padding: EdgeInsets.all(8.w),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(8.r),
+                    border: Border.all(
+                      width: 0.5.w,
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
