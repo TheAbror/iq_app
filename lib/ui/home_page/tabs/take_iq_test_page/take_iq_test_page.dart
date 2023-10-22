9c72f5bd3b19338135f0b68eca3e3b14992d44b1
Elbarto <oyjddkvj@gmail.com>
2023-10-22 21:15:26 +0500

    connected to backend successfully

--- lib/ui/home_page/tabs/take_iq_test_page/take_iq_test_page.dart
+++ lib/ui/home_page/tabs/take_iq_test_page/take_iq_test_page.dart
@@ -15,95 +15,119 @@ class TakeIQTest extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: BlocProvider(
-          create: (context) => QuestionsBloc()..getQuestions(),
-          child: BlocBuilder<QuestionsBloc, QuestionsState>(
-            builder: (context, state) {
-              if (state.blocProgress == BlocProgress.IS_LOADING) {
-                return const PrimaryLoader();
-              }
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: BlocProvider(
+            create: (context) => QuestionsBloc()..getQuestions(),
+            child: BlocBuilder<QuestionsBloc, QuestionsState>(
+              builder: (context, state) {
+                if (state.blocProgress == BlocProgress.IS_LOADING) {
+                  return const PrimaryLoader();
+                }
 
-              if (state.blocProgress == BlocProgress.FAILED) {
-                return const SomethingWentWrong();
-              }
-              return Column(
-                children: [
-                  SizedBox(height: 30.h),
-                  Container(
-                    height: 300,
-                    width: double.infinity,
-                    padding: EdgeInsets.all(8.w),
-                    decoration: BoxDecoration(
-                      color: AppColors.textMain,
-                      borderRadius: BorderRadius.circular(8.r),
-                      border: Border.all(),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'questions[counter].question',
-                        style: const TextStyle(
-                          color: AppColors.float,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                        ),
-                        textAlign: TextAlign.center,
+                if (state.blocProgress == BlocProgress.FAILED) {
+                  return const SomethingWentWrong();
+                }
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 600,
+                      child: ListView.builder(
+                        itemCount: state.questions.length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                padding: EdgeInsets.all(8.w),
+                                decoration: BoxDecoration(
+                                  color: AppColors.textMain,
+                                  borderRadius: BorderRadius.circular(8.r),
+                                  border: Border.all(),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    state.questions[index].question,
+                                    style: const TextStyle(
+                                      color: AppColors.float,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              state.questions[index].options[index].isCorrect
+                                  ? Text(
+                                      state.questions[index].options[index].option,
+                                      style: const TextStyle(
+                                        color: AppColors.emirald,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    )
+                                  : Text('incorrect')
+                            ],
+                          );
+                        },
                       ),
                     ),
-                  ),
-                  SizedBox(height: 40.h),
-                  // ListView.builder(
-                  //   shrinkWrap: true,
-                  //   padding: EdgeInsets.zero,
-                  //   itemCount: questions[0].options.length,
-                  //   physics: const NeverScrollableScrollPhysics(),
-                  //   itemBuilder: (context, index) {
-                  //     return ListTile(
-                  //       contentPadding: EdgeInsets.zero,
-                  //       title: AnwerItem(
-                  //         letter: '',
-                  //         text: questions[counter].options[index],
-                  //       ),
-                  //       onTap: () {
-                  //         if (questions[counter].options[index] == questions[counter].answer) {
-                  //           print('correct');
-                  //           icons.add(
-                  //             Icon(
-                  //               Icons.done,
-                  //               color: Colors.green.shade800,
-                  //             ),
-                  //           );
-                  //         } else {
-                  //           print('wrong');
-                  //           icons.add(
-                  //             Icon(
-                  //               Icons.exposure_minus_1_outlined,
-                  //               color: Colors.red.shade800,
-                  //             ),
-                  //           );
-                  //         }
-                  //         if (counter + 1 < questions.length) {
-                  //           setState(() {
-                  //             counter++;
-                  //           });
-                  //         }
-                  //       },
-                  //     );
-                  //   },
-                  // ),
-                  SizedBox(height: 40.h),
-                  // Row(
-                  //   crossAxisAlignment: CrossAxisAlignment.end,
-                  //   children: icons,
-                  // ),
-                  // if (icons.length == questions.length) const Text('Your result: ')
-                ],
-              );
-            },
+                    SizedBox(height: 30.h),
+
+                    SizedBox(height: 40.h),
+                    // ListView.builder(
+                    //   shrinkWrap: true,
+                    //   padding: EdgeInsets.zero,
+                    //   itemCount: questions[0].options.length,
+                    //   physics: const NeverScrollableScrollPhysics(),
+                    //   itemBuilder: (context, index) {
+                    //     return ListTile(
+                    //       contentPadding: EdgeInsets.zero,
+                    //       title: AnwerItem(
+                    //         letter: '',
+                    //         text: questions[counter].options[index],
+                    //       ),
+                    //       onTap: () {
+                    //         if (questions[counter].options[index] == questions[counter].answer) {
+                    //           print('correct');
+                    //           icons.add(
+                    //             Icon(
+                    //               Icons.done,
+                    //               color: Colors.green.shade800,
+                    //             ),
+                    //           );
+                    //         } else {
+                    //           print('wrong');
+                    //           icons.add(
+                    //             Icon(
+                    //               Icons.exposure_minus_1_outlined,
+                    //               color: Colors.red.shade800,
+                    //             ),
+                    //           );
+                    //         }
+                    //         if (counter + 1 < questions.length) {
+                    //           setState(() {
+                    //             counter++;
+                    //           });
+                    //         }
+                    //       },
+                    //     );
+                    //   },
+                    // ),
+                    SizedBox(height: 40.h),
+                    // Row(
+                    //   crossAxisAlignment: CrossAxisAlignment.end,
+                    //   children: icons,
+                    // ),
+                    // if (icons.length == questions.length) const Text('Your result: ')
+                  ],
+                );
+              },
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
